b6928bfc7414e4c2b85649f650eba2e526cb8c12
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-07-22 22:00:04 -0600

    crear usuario

--- Usuarios/crear_usuarios.php
+++ Usuarios/crear_usuarios.php
@@ -1,17 +1,32 @@
 <?php 
 
 include "../conexion.php";
-
-
 $postdata = file_get_contents("php://input"); 
-$data = json_decode($postdata);
  
-$query = "SELECT  public.ft_insertar_usuario('".$postdata."')";
+$sql_query = "SELECT  public.ft_insertar_usuario('".$postdata."')";
 
-$resultado = pg_query($db_handle,$query);
-
-$datos = pg_fetch_all($resultado);
-echo $datos[0]["ft_insertar_usuario"];
-
+if (pg_send_query($con, $sql_query)) {
+    $res=pg_get_result($con);
+    if ($res) {
+      $state = pg_result_error_field($res, PGSQL_DIAG_SQLSTATE);
+      if ($state==0) {
+            $datos = pg_fetch_all($res);
+            $variable = $datos[0]["ft_insertar_usuario"]; 
+            echo $variable ;   
+            }
+      else {
+        // some error happened
+        if ($state == 23505) { // unique_violation
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;    
+        }
+        else {
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;
+         // process other errors
+        }
+      }
+    }  
+  }
 
 ?>
