974b49330c25936cb6918725fde33fe1b55dc5eb
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-07-22 21:44:45 -0600

    fix login

--- login.php
+++ login.php
@@ -4,9 +4,6 @@ header("Access-Control-Allow-Headers: *");
 include "conexion.php";
 $postdata = file_get_contents("php://input"); 
 
-$nombre = $_GET["nombre"];
-$contra = $_GET["contra"];
-
 $query = "SELECT public.ft_login('".$postdata."')";
 
 if (pg_send_query($con, $sql_query)) {
