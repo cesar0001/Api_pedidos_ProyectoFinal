dd0d87c6251c4ce04f4b7520c2a45f2d974e01d1
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-07-28 23:02:46 -0600

    insertPedido

---
+++ pedidos/insertProductoPedido.php
@@ -0,0 +1,33 @@
+<?php 
+
+include "../conexion.php";
+$postdata = file_get_contents("php://input"); 
+ 
+$sql_query = "SELECT  public.ft_proc_insert_pedidos('".$postdata."')";
+
+if (pg_send_query($con, $sql_query)) {
+    $res=pg_get_result($con);
+    if ($res) {
+      $state = pg_result_error_field($res, PGSQL_DIAG_SQLSTATE);
+      if ($state==0) {
+            $datos = pg_fetch_all($res);
+            $variable = (boolean)$datos[0]["ft_proc_insert_pedidos"];
+            $resultado = array('estado' => $variable);
+            echo json_encode($resultado) ;    
+            }
+      else {
+        // some error happened
+        if ($state == 23505) { // unique_violation
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;    
+        }
+        else {
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;
+         // process other errors
+        }
+      }
+    }  
+  }
+
+?>
