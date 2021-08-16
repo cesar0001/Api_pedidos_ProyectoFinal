adcbbbc169864987603bb8e0d81414474341f3c6
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-08-15 22:53:32 -0600

    get_estadisticas_dias

---
+++ pedidos/getEstadisticasDias.php
@@ -0,0 +1,35 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+include "../conexion.php";
+
+$postdata = file_get_contents("php://input"); 
+
+$sql_query = "SELECT  public.get_estadisticas_dias()";
+
+if (pg_send_query($con, $sql_query)) {
+    $res=pg_get_result($con);
+    if ($res) {
+      $state = pg_result_error_field($res, PGSQL_DIAG_SQLSTATE);
+      if ($state==0) {
+            $datos = pg_fetch_all($res);
+            $variable = $datos[0]["get_estadisticas_dias"];
+            // $resultado = array('estado' => $variable);
+            echo $variable ;    
+            }
+      else {
+        // some error happened
+        if ($state == 23505) { // unique_violation
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;    
+        }
+        else {
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;
+         // process other errors
+        }
+      }
+    }  
+  }
+
+?>
